8b4d8288c62a3a65148061d346ced79dc7afda44
Geraldine202 <geraldineinostroza1995@gmail.com>
2024-09-16 18:08:24 -0300

    detalle viajes en crud

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -40,13 +40,14 @@ const routes: Routes = [
     loadChildren: () => import('./pages/portada/portada.module').then( m => m.PortadaPageModule)
   },
   {
-    path: '**',
-    loadChildren: () => import('./pages/error404/error404.module').then( m => m.Error404PageModule)
-  },  {
     path: 'detalles-viaje',
     loadChildren: () => import('./pages/detalles-viaje/detalles-viaje.module').then( m => m.DetallesViajePageModule)
   },
 
+  {
+    path: '**',
+    loadChildren: () => import('./pages/error404/error404.module').then( m => m.Error404PageModule)
+  },
 ];
 
 @NgModule({
