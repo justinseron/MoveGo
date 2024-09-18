6349cd1285a60c71306c8bc188d86d3c32afbd15
Geraldine202 <geraldineinostroza1995@gmail.com>
2024-09-17 21:25:37 -0300

    recuperar

--- src/app/pages/recuperar/recuperar.page.ts
+++ src/app/pages/recuperar/recuperar.page.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ModalController } from '@ionic/angular';
+import { VerificarCodigoPage } from '../verificar-codigo/verificar-codigo.page';
 
 @Component({
   selector: 'app-recuperar',
@@ -7,9 +9,23 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RecuperarPage implements OnInit {
 
-  constructor() { }
+  //ngModel
+  email: string = "";
+
+  constructor(private modalController: ModalController) { }
 
   ngOnInit() {
+  }
+
+  async mostrarModalVerificacion(){
+    const modal = await this.modalController.create({
+      component: VerificarCodigoPage
+    });
+    return await modal.present();
+  }
+
+  recuperarContrasena(){
+    this.mostrarModalVerificacion();
   }
 
 }
