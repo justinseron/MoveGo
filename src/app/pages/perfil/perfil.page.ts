8305787fffbd3170f9b75c1e2c7739b8447575be
justinseron <ju.seron@duocuc.cl>
2024-09-23 14:20:12 -0300

    Cerrar sesión en PerfilPage (crud_usuario)

--- src/app/pages/perfil/perfil.page.ts
+++ src/app/pages/perfil/perfil.page.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { AlertController } from '@ionic/angular';
 
 @Component({
   selector: 'app-perfil',
@@ -7,9 +9,38 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PerfilPage implements OnInit {
 
-  constructor() { }
+  constructor(private router: Router, private alertController: AlertController) { }
 
   ngOnInit() {
+  }
+
+  async confirmarCerrar(){
+    const alert = await this.alertController.create({
+      header: 'Confirmar Cierre de Sesión',
+      message: '¿Está seguro de que desea cerrar la sesión?',
+      buttons: [
+        {
+          text: 'Cancelar',
+          role: 'cancel',
+          handler: () => {
+            //No hacer NADA si cancela
+          }
+        },
+        {
+          text: 'Aceptar',
+          handler: () => {
+            this.cerrarSesion(); //Llama a la función si se acepta
+          }
+        }
+      ]
+    });
+
+    await alert.present(); //Mostramos la alerta
+  }
+
+  //Cerrar sesión provisorio
+  cerrarSesion(){
+    this.router.navigate(['/login']);
   }
 
 }
